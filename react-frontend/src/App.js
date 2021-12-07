cd56ab2c3c669f704449a058f8fe7b42dcd77399
cristian0x <christianbialek@wp.pl>
2021-12-07 16:29:25 +0100

    initial commit

---
+++ react-frontend/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import ListEmployee from "./components/ListEmployee";
+
+function App() {
+  return (
+    <div>
+      <div>SimpleSpringBootReactApp</div>
+      <ListEmployee />
+    </div>
+  );
+}
+
+export default App;
